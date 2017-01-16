0443d13ef722dbb237d3d88968a6393028473453
Rian Wardana <rian@rianwardana.com>
2017-01-16 21:51:52 +0700

    Move to rianwardana.com

--- application/views/home_jadwal_view.php
+++ application/views/home_jadwal_view.php
@@ -27,7 +27,7 @@
 		<script>
 			$(document).ready(function(){
 				$.ajax({
-					url: 'http://jadwal.bookingtst.com/api/kelas/' + <?php echo $session_kelas; ?>,
+					url: 'http://jadwalgo.rianwardana.com/api/kelas/' + <?php echo $session_kelas; ?>,
 					success: function(string) {
 						row = JSON.parse(string);
 						$.each(row, function(i, kelas){
@@ -38,7 +38,7 @@
 				})
 
 				$.ajax({
-					url: 'http://jadwal.bookingtst.com/api/cari/' + <?php echo $session_kelas; ?>,
+					url: 'http://jadwalgo.rianwardana.com/api/cari/' + <?php echo $session_kelas; ?>,
 					success: function(string) {
 						row = JSON.parse(string);
 						var html = '';
